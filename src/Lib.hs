c68a70191c0e48b164cd92a0b62df655434a6c71
Steve Mostovoy <stevemostovoysm@gmail.com>
2020-06-04 17:11:02 -0700

    Init and print the board.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,24 +1,50 @@
 module Lib (chess) where
 import Data.Array
 
-width :: Int
+width :: Integer
 width = 8
 
-height :: Int
+height :: Integer
 height = 8
 
-data State = Empty deriving (Eq, Show)
-type Position = (Int, Int)
+data State = Empty |
+              WhitePawn | WhiteKnight | WhiteBishop | WhiteRook | WhiteQueen | WhiteKing |
+              BlackPawn | BlackKnight | BlackBishop | BlackRook | BlackQueen | BlackKing
+              deriving (Eq, Show)
+type Position = (Integer, Integer)
 type Board = Array Position State
 
 initBoard :: Position -> Board
 initBoard (width, height) =
   let bounds = ((0, 0), (width - 1, height - 1))
-  in array bounds $ zip (range bounds) (repeat Empty)
+  in array bounds $ zip (range bounds) [
+    BlackRook, BlackBishop, BlackKnight, BlackQueen, BlackKing, BlackBishop, BlackKnight, BlackRook,
+    BlackPawn, BlackPawn,   BlackPawn,   BlackPawn,  BlackPawn, BlackPawn,   BlackPawn,   BlackPawn,
+    Empty,     Empty,       Empty,       Empty,      Empty,     Empty,       Empty,       Empty,
+    Empty,     Empty,       Empty,       Empty,      Empty,     Empty,       Empty,       Empty,
+    Empty,     Empty,       Empty,       Empty,      Empty,     Empty,       Empty,       Empty,
+    Empty,     Empty,       Empty,       Empty,      Empty,     Empty,       Empty,       Empty,
+    WhitePawn, WhitePawn,   WhitePawn,   WhitePawn,  WhitePawn, WhitePawn,   WhitePawn,   WhitePawn,
+    WhiteRook, WhiteKnight, WhiteBishop, WhiteQueen, WhiteKing, WhiteBishop, WhiteKnight, WhiteRook]
+
+showPiece :: State -> String
+showPiece Empty = " "
+showPiece WhitePawn   = "\x1b[37mP\x1b[0m"
+showPiece WhiteKnight = "\x1b[37mN\x1b[0m"
+showPiece WhiteBishop = "\x1b[37mB\x1b[0m"
+showPiece WhiteRook   = "\x1b[37mR\x1b[0m"
+showPiece WhiteQueen  = "\x1b[37mQ\x1b[0m"
+showPiece WhiteKing   = "\x1b[37mK\x1b[0m"
+showPiece BlackPawn   = "\x1b[90mP\x1b[0m"
+showPiece BlackKnight = "\x1b[90mN\x1b[0m"
+showPiece BlackBishop = "\x1b[90mB\x1b[0m"
+showPiece BlackRook   = "\x1b[90mR\x1b[0m"
+showPiece BlackQueen  = "\x1b[90mQ\x1b[0m"
+showPiece BlackKing   = "\x1b[90mK\x1b[0m"
 
 printArray :: Board -> String
 printArray arr =
-  unlines [unwords [show (arr ! (x, y)) | x <- [0..width - 1]] | y <- [0..height - 1]]
+  unlines [unwords [showPiece (arr ! (y, x)) | x <- [0..width - 1]] | y <- [0..height - 1]]
 
 chess :: IO ()
 chess = do
